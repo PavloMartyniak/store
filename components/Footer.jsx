4ca1de5450ba01ed4cdcea904ca1e31419af9ad0
PavloMartyniak <pavlomartyniak.work@gmail.com>
2024-02-27 17:54:47 +0200

    created header navigation modal

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { navigation_pages } from "@/utils/navigation_pages";
 
 const Footer = () => {
-  return <div className="w-full bg-dark_grey min-h-[5vh]"></div>;
+  return (
+    <div className="w-full bg-dark_grey min-h-[5vh] flex items-center gap-10 py-10">
+      {navigation_pages.map((page) => (
+        <div>
+          {page.children_pages && (
+            <h1 className="font-bold text-xl cursor-pointer hover:text-white">
+              {page.title}
+            </h1>
+          )}
+          {page.children_pages?.map((item) => (
+            <div className="cursor-pointer hover:text-white">{item.title}</div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Footer;
